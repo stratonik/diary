5dfce7dc3dec3df5259b433b4151e2beae6f7b22
Alexandr Belitsky <stratonik@gmail.com>
2013-12-25 16:18:52 +0400

    Доработан немного интерфейс

---
+++ src/ru/abelitsky/diary/client/views/resources/Resources.java
@@ -0,0 +1,11 @@
+package ru.abelitsky.diary.client.views.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface Resources extends ClientBundle {
+
+	@Source("edit-icon-small.png")
+	ImageResource editIcon();
+
+}
